0292d731ed78fc78f0f07b16dc4b209e4b99d1de
Peter de Boer <peterdeboer_private@hotmail.com>
2020-12-07 12:07:17 +0000

    fixed uptime format

--- src/format.cpp
+++ src/format.cpp
@@ -3,6 +3,7 @@
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
 // DONE: Complete this helper function
 // INPUT: Long int measuring seconds
@@ -11,5 +12,13 @@ string Format::ElapsedTime(long seconds) {
   int hours = seconds / 3600;
   int minutes = (seconds % 3600) / 60;
   int sec = seconds % 60;
-  return std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(sec);
+  
+  string prev_hh{""};
+  if (hours < 10) {prev_hh = "0";}
+  string prev_mm{""};
+  if (minutes < 10) {prev_mm = "0";}
+  string prev_ss{""};
+  if (sec < 10) {prev_ss = "0";}
+  
+  return prev_hh + to_string(hours) + ":" + prev_mm + std::to_string(minutes) + ":" + prev_ss + std::to_string(sec);
 }
